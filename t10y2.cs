c1e801ee1d378224f888c9189c3ae4bf9e41e174
Dexter0339 <136196150+Dexter0339@users.noreply.github.com>
2023-11-19 17:45:27 +0200

    Add files via upload

---
+++ t10y2.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите значение катета: ");
+double a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение гипотенузы: ");
+double c = int.Parse(Console.ReadLine());
+double b = Math.Sqrt((c * c) - (a * a));
+Console.WriteLine("Значение второго катета: " + b);
